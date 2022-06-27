d94ca14c06be4e5208bb571b370b322ec88091cc
yiyun <yiyungent@gmail.com>
2022-06-27 21:14:34 +0800

    docs(src/coo/): iToday

---
+++ src/coo/Models/IToday/ITodayGetDateRecordResponseModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coo.Models.IToday
+{
+    public class ITodayGetDateRecordResponseModel
+    {
+        public DataModel data { get; set; }
+
+        public string info { get; set; }
+
+        public int status { get; set; }
+
+        public class DataModel
+        {
+            public ItemModel[] items { get; set; }
+
+            public class ItemModel
+            {
+                public long id { get; set; }
+                public long userId { get; set; }
+                public long goalId { get; set; }
+                public long goalType { get; set; }
+                public string startTime { get; set; }
+                public long take { get; set; }
+                public string stopTime { get; set; }
+                public int isEnd { get; set; }
+                public long isRecord { get; set; }
+                public string remarks { get; set; }
+                public int isDelete { get; set; }
+                public string deleteTime { get; set; }
+                public string endUpdateTime { get; set; }
+            }
+        }
+    }
+}
---
+++ src/coo/Utils/ITodayApiUtil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coo.Utils
+{
+    /// <summary>
+    /// https://api.timefriend.vip/ApiV2/
+    /// </summary>
+    public class ITodayApiUtil
+    {
+        public void Login(string userName, string password)
+        {
+
+        }
+
+        public Models.IToday.ITodayGetDateRecordResponseModel GetDateRecord(DateTime time)
+        {
+            Models.IToday.ITodayGetDateRecordResponseModel responseModel = new Models.IToday.ITodayGetDateRecordResponseModel();
+            try
+            {
+                string tokenStr = "";
+                string activity_flagStr = "112207048";
+                string timeStr = time.ToString("yyyy-MM-dd 00:00:00");
+
+            }
+            catch (Exception ex)
+            {
+                LogUtil.Exception(ex);
+            }
+
+            return responseModel;
+        }
+    }
+}
