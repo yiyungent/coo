a87290e7b7d7404f938457ea9dd02f55a37e72e5
yiyun <yiyungent@gmail.com>
2023-05-21 19:42:50 +0800

    docs(src/enexlib/): update

---
+++ src/EnexLib/XmlEntity/ApplicationData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EnexLib.XmlEntity
+{
+    public class ApplicationData
+    {
+        [XmlAttribute("key")]
+        public string Key { get; set; }
+
+        [XmlText]
+        public string Value { get; set; }
+    }
+}
